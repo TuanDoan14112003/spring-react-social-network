c748ad887a73b52ab9e0e3e8f4acca5ba70328d2
Tuan Doan <tuandoan14112003@gmail.com>
2023-07-09 15:53:46 +1000

    feat: set up spring-boot project

---
+++ backend/src/test/java/com/softwaredevelopment/socialnetworkapp/SocialNetworkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwaredevelopment.socialnetworkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
