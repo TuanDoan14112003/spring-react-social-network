9aec4bfe6ce7e551d0deb38ea8b091f05b8a21ae
Tuan Doan <tuandoan14112003@gmail.com>
2023-07-11 15:45:42 +1000

    feat: create UserRepository

---
+++ backend/src/main/java/dao/UserRepository.java
@@ -0,0 +1,14 @@
+package dao;
+
+import com.softwaredevelopment.socialnetworkapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
+}
