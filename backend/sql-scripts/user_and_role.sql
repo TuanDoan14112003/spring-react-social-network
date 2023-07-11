f168406a61dfd36533fccfae67bbec34bf56539f
Tuan Doan <tuandoan14112003@gmail.com>
2023-07-11 15:32:57 +1000

    feat: add new account_role table

--- backend/sql-scripts/user_and_role.sql
+++ backend/sql-scripts/user_and_role.sql
@@ -8,8 +8,15 @@ CREATE TABLE IF NOT EXISTS account (
     dob DATE,
     active BOOLEAN NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS role (
-    user_id INTEGER NOT NULL,
-    role VARCHAR(20) NOT NULL
+    role_name VARCHAR(20) NOT NULL PRIMARY KEY
 );
+
+CREATE TABLE IF NOT EXISTS account_role (
+    account_id INTEGER NOT NULL,
+    role VARCHAR(20) NOT NULL,
+    PRIMARY KEY (account_id,role),
+    FOREIGN KEY (account_id) references account(id),
+    FOREIGN KEY (role) references role(role_name)
+);
+
