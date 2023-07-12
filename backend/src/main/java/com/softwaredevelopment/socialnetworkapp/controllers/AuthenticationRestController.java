5bd2fda63d784787771a1868798e055b9407ae41
Tuan Doan <tuandoan14112003@gmail.com>
2023-07-12 17:07:38 +1000

    feat: add AuthenticationRestController class

---
+++ backend/src/main/java/com/softwaredevelopment/socialnetworkapp/controllers/AuthenticationRestController.java
@@ -0,0 +1,91 @@
+package com.softwaredevelopment.socialnetworkapp.controllers;
+
+
+import com.softwaredevelopment.socialnetworkapp.model.*;
+import com.softwaredevelopment.socialnetworkapp.repositories.RoleRepository;
+import com.softwaredevelopment.socialnetworkapp.repositories.UserRepository;
+import com.softwaredevelopment.socialnetworkapp.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationRestController {
+
+    AuthenticationManager authenticationManager;
+    UserRepository userRepository;
+    RoleRepository roleRepository;
+    PasswordEncoder encoder;
+    JwtUtils jwtUtils;
+
+    @Autowired
+    public AuthenticationRestController(AuthenticationManager authenticationManager,UserRepository userRepository,RoleRepository roleRepository, PasswordEncoder encoder, JwtUtils jwtUtils) {
+        this.userRepository =userRepository;
+        this.roleRepository = roleRepository;
+        this.authenticationManager = authenticationManager;
+        this.encoder = encoder;
+        this.jwtUtils = jwtUtils;
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LogInRequest loginRequest) {
+
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        User userDetails = (User) authentication.getPrincipal();
+
+        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
+
+
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .body(new UserInfoResponse(userDetails.getId(),
+                        userDetails.getUsername(),
+                        userDetails.getEmail()
+                        ));
+
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody SignUpRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity.badRequest().body(new SimpleMessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity.badRequest().body(new SimpleMessageResponse("Error: Email is already in use!"));
+        }
+
+
+        User user = new User(signUpRequest.getEmail(),
+                signUpRequest.getUsername(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new SimpleMessageResponse("User registered successfully!"));
+    }
+
+    @PostMapping("/signout")
+    public ResponseEntity<?> logoutUser() {
+        ResponseCookie cookie = jwtUtils.getCleanJwtCookie();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(new SimpleMessageResponse("You've been signed out!"));
+    }
+}
