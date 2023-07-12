04eacd73f41e4933b4f089de01358d152a8788e9
Tuan Doan <tuandoan14112003@gmail.com>
2023-07-12 17:08:43 +1000

    fix: change rest folder name to controllers

--- backend/src/main/java/com/softwaredevelopment/socialnetworkapp/controllers/HelloWorldRestController.java
+++ backend/src/main/java/com/softwaredevelopment/socialnetworkapp/controllers/HelloWorldRestController.java
@@ -1,4 +1,4 @@
-package com.softwaredevelopment.socialnetworkapp.rest;
+package com.softwaredevelopment.socialnetworkapp.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
