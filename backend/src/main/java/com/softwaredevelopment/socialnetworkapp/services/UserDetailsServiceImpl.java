33f60a3fc4722a46e62fcd60f187ec62258043d3
Tuan Doan <tuandoan14112003@gmail.com>
2023-07-12 16:50:06 +1000

    feat: add UserDetailsServiceImpl

---
+++ backend/src/main/java/com/softwaredevelopment/socialnetworkapp/services/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.softwaredevelopment.socialnetworkapp.services;
+
+import com.softwaredevelopment.socialnetworkapp.repositories.UserRepository;
+import com.softwaredevelopment.socialnetworkapp.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
+
+        return user;
+    }
+
+}
