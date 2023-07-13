ea1c6b9a45bab04dbc2eca7d010dfd6578aa6e4b
kiennguyen2403 <kientqk2403@example.com>
2023-07-13 21:40:51 +1000

    feature: first commit, google auth

--- backend/src/main/java/com/softwaredevelopment/socialnetworkapp/controllers/AuthenticationRestController.java
+++ backend/src/main/java/com/softwaredevelopment/socialnetworkapp/controllers/AuthenticationRestController.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -58,6 +59,18 @@ public class AuthenticationRestController {
                 .body(userDetails);
 
     }
+
+    @GetMapping("/auth/google")
+    public String googleAuth() {
+        return "redirect:/auth/google";
+    }
+    
+    @GetMapping("/auth/google/callback")
+    public String googleCallback() {
+        return "redirect:/";
+    }
+
+
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody SignUpRequest signUpRequest) {
