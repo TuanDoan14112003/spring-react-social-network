de24656308e28075b117efde7384015dc03aecb9
kiennguyen2403 <kientqk2403@example.com>
2023-07-21 15:42:26 +1000

    feat: adding image button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import Navigation from './navigation/navigation';
 function App() {
   return (
     <div style={{ width: "100%", height: "100%" }}>
-      <Navigation />
+  
     </div>
   )
 
---
+++ frontend/src/page-components/image-button.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Card, CardActionArea, Typography } from "@mui/material";
+
+interface ImageButtonProps {
+    eventHandler: () => void;
+    image: string;
+    text: string;
+}
+
+const ImageButton: React.FC<ImageButtonProps> = ({
+    eventHandler,
+    image,
+    text
+}) => {
+    const buttonStyle = {
+        background: `url(${image})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '20.75rem',
+        height: '13rem',
+    };
+    return (
+        <Card style={buttonStyle} onClick={eventHandler}>
+            <CardActionArea sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+                <Typography style={{
+                    color: '#FFF',
+                    textAlign: 'center',
+                    fontFamily: 'Rajdhani',
+                    fontSize: '1.25rem',
+                    fontStyle: 'normal',
+                    fontWeight: 700,
+                    lineHeight: '130%',
+                }}>
+                    {text}
+                </Typography>
+            </CardActionArea>
+        </Card >
+
+    )
+
+}
+
+export default ImageButton;
